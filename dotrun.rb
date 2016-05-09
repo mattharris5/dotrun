5454d0534b832a8ffdd2d7fa938df9aaf4892d6c
Matt Harris <matt@dreamsis.com>
2016-05-09 10:04:46 -0700

    Print the list of available directives with `dotrun -?`

--- dotrun.rb
+++ dotrun.rb
@@ -6,8 +6,20 @@ directives_path = ".run"
 abort("No .run file detected in current directory.\r\n".red) unless File.exist?(directives_path)
 directives = YAML.load_file(directives_path)
 
+def print_help_and_exit(directives)
+  puts "Usage: dotrun [directive]"
+  print "Available directives for ".blue
+  print "#{File.basename(File.expand_path("."))}".blue.bold
+  puts ":".blue
+  directives = { default: directives } if directives.is_a?(String)
+  directives.each{|key, command| puts "  #{key.to_s.ljust(10).magenta} #{"->".light_black} #{command}"}
+  puts ""
+  exit
+end
+
 directive = ARGV[0] || "default"
 command = directives.is_a?(String) ? directives : directives[directive]
+print_help_and_exit(directives) if directive == "-?"
 abort("Couldn't find the `#{directive}` directive in your config.\r\n".red) unless command
 
 system("clear")
